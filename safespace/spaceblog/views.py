738653fe9820a79ba7f12cc8bfcc4308fdaf07d2
Mr Uzo <chrisuzoewulu@gmail.com>
2020-05-16 17:16:28 -0400

    password reset

--- safespace/spaceblog/views.py
+++ safespace/spaceblog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Post, Like
 from django.http import HttpResponse
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # Create your views here.
 
@@ -8,6 +10,54 @@ from django.http import HttpResponse
 def index(request):
     posts = Post.objects.all()
     return render(request, 'spaceblog/index.html', {'posts': posts})
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'spaceblog/index.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['post_heading', 'post_text']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['post_heading', 'post_text']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/spaceblog'
+
+    def test_func(self):
+        post = self.get_object()
+        # print(dir(post))
+        if self.request.user == post.author:
+            return True
+        return False
+
 
 def likePost(request):
     if request.method == 'GET':
